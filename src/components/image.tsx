c18880667770f7051d7c41ec06206227fbfb19a2
Maulon <tmaulon@betomorrow.com>
2020-01-20 09:40:26 +0100

    refactor components

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -13,20 +13,18 @@ import Img from "gatsby-image"
  * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-const Image = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-    }
-  `)
+export const Image = () => {
+	const data = useStaticQuery(graphql`
+		query {
+			placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+				childImageSharp {
+					fluid(maxWidth: 300) {
+						...GatsbyImageSharpFluid
+					}
+				}
+			}
+		}
+	`)
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+	return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
 }
-
-export default Image
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,11 +6,11 @@
  */
 
 import React, { ReactNode } from "react"
-import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import { Header } from "./header"
 import "./layout.css"
+import { Footer } from "./footer"
 
 export const Layout = ({ children }: { children: ReactNode }) => {
 	const data = useStaticQuery(graphql`
@@ -26,21 +26,8 @@ export const Layout = ({ children }: { children: ReactNode }) => {
 	return (
 		<>
 			<Header siteTitle={data.site.siteMetadata.title} />
-			<div
-				style={{
-					margin: `0 auto`,
-					maxWidth: 960,
-					padding: `0px 1.0875rem 1.45rem`,
-					paddingTop: 0,
-				}}
-			>
-				<main>{children}</main>
-				<footer>
-					© {new Date().getFullYear()}, Built with
-					{` `}
-					<a href="https://www.gatsbyjs.org">Gatsby</a>
-				</footer>
-			</div>
+			<main>{children}</main>
+			<Footer />
 		</>
 	)
 }
